f4f9ea1e0ea3c02070fdc2506177270adc87a9de
Pawel Galusza <pawelgalka153@gmail.com>
2026-06-08 22:44:36 +0200

    add ghost model of manipulator to show difference between target and current orientation

---
+++ ControlPanel.Domain/Entities/UrdfVisualiser/ActuatorAndJointNamePair.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlPanel.Domain.Entities.UrdfVisualiser
+{
+    public class ActuatorAndJointNamePair
+    {
+        public Actuator? Acutator { get; set; }
+        public string JointName { get; set; } = "";
+
+        public ActuatorAndJointNamePair(Actuator? actuator, string jointName) {
+            Acutator = actuator;
+            JointName = jointName;
+        }
+    }
+
+
+}
--- ControlPanel.WPF/ViewModels/RobotVisualiser3DViewModel.cs
+++ ControlPanel.WPF/ViewModels/RobotVisualiser3DViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using ControlPanel.Application.Interfaces;
+using ControlPanel.Domain.Entities;
 using ControlPanel.Domain.Entities.UrdfVisualiser;
 using ControlPanel.Domain.ValueObjects.UrdfVisualiser;
 using ControlPanel.Presentation.WPF.Common;
@@ -9,6 +10,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Numerics;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Media3D;
 
 namespace ControlPanel.Presentation.WPF.ViewModels
@@ -22,8 +24,10 @@ namespace ControlPanel.Presentation.WPF.ViewModels
 
         private UrdfRobot? _robot;
         private readonly Dictionary<string, Model3DGroup> _linkGroups = new();
+        private readonly Dictionary<string, Model3DGroup> _ghostLinkGroups = new();
 
         public Model3DGroup RobotScene { get; } = new();
+        public Model3DGroup GhostScene { get; } = new();
         
         public RobotVisualiser3DViewModel(
             IUrdfLoader urdfLoader, 
@@ -38,12 +42,14 @@ namespace ControlPanel.Presentation.WPF.ViewModels
             _userInteractionService = userInteractionService;
 
             _robotStateService.StateUpdated += OnRobotStateUpdated;
+            _robotStateService.RobotConfigured += OnRobotConfiguredUpdated;
 
             LoadUrdfCommand = new RelayCommand(LoadUrdf);
         }
         public override void Dispose()
         {
             _robotStateService.StateUpdated -= OnRobotStateUpdated;
+            _robotStateService.RobotConfigured -= OnRobotConfiguredUpdated;
             base.Dispose();
         }
         public ObservableCollection<LinkDebugInfo> LinkInfos { get; } = new();
@@ -74,6 +80,7 @@ namespace ControlPanel.Presentation.WPF.ViewModels
                 BuildScene();
                 ComputeAndApplyTransforms();
                 StatusText = $"{Path.GetFileName(path)} | has: {_robot.Links.Count} links and {CountMovable()} DOF";
+                SetupDefaultActuatorToJointMapping();
             }
             catch (Exception ex)
             {
@@ -88,36 +95,71 @@ namespace ControlPanel.Presentation.WPF.ViewModels
 
             _linkGroups.Clear();
             RobotScene.Children.Clear();
+            GhostScene.Children.Clear();
 
             //add roots to scene
             foreach (string rootName in _robot.GetRootLinksNames())
             {
                 if (!_robot.Links.TryGetValue(rootName, out var link)) continue;
+                
                 var group = CreateLinkGroup(link);
                 _linkGroups[rootName] = group;
                 RobotScene.Children.Add(group);
+
+                var ghostGroup = CreateLinkGroup(link, true);
+                _ghostLinkGroups[rootName] = ghostGroup;
+                GhostScene.Children.Add(ghostGroup);
             }
 
             //add children for each joint
             foreach (UrdfJoint joint in _robot.Joints.Values) {
                 if(!_robot.Links.TryGetValue(joint.ChildLinkName, out var link)) continue;
+                
                 var group = CreateLinkGroup(link);
                 _linkGroups[joint.Name] = group;
                 RobotScene.Children.Add(group);
-            
+
+                var ghostGroup = CreateLinkGroup(link, true);
+                _ghostLinkGroups[joint.Name] = ghostGroup;
+                GhostScene.Children.Add(ghostGroup);
+
             }
         }
 
-        private static Model3DGroup CreateLinkGroup(UrdfLink link)
+        private static Model3DGroup CreateLinkGroup(UrdfLink link, bool isGhost=false)
         {
             Model3DGroup? group = new Model3DGroup();
             string? path = link.Visual?.MeshFileAbsolutePath;
             if (path != null && path.Length > 0){
                 var mesh = TryLoadMesh(path);
                 if(mesh != null)
+                {
+                    if (isGhost)
+                        ApplyGhostMaterial(mesh);
                     group.Children.Add(mesh);
+                }
             }
             return group;
+        }
+
+        private static void ApplyGhostMaterial(Model3D mesh)
+        {
+            //if (mesh is not GeometryModel3D geo) return;
+            //geo.Material = new DiffuseMaterial(new SolidColorBrush(Color.FromArgb(80, 0, 150, 255)));
+            //geo.BackMaterial = geo.Material;
+
+            if (mesh is Model3DGroup group)
+            {
+                foreach (var child in group.Children)
+                    ApplyGhostMaterial(child);
+                return;
+            }
+
+            if (mesh is not GeometryModel3D geo) return;
+
+            var brush = new SolidColorBrush(Color.FromArgb(80, 0, 150, 255));
+            geo.Material = new DiffuseMaterial(brush);
+            geo.BackMaterial = geo.Material;
         }
 
         private static Model3D? TryLoadMesh(string path)
@@ -139,6 +181,78 @@ namespace ControlPanel.Presentation.WPF.ViewModels
             }
         }
 
+        private List<Actuator> _availableActuators = new();
+        public List<Actuator> AvailableActuators
+        {
+            get => _availableActuators;
+            set
+            {
+                _availableActuators = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private List<ActuatorAndJointNamePair> _pairedActuatorAndJoints = new();
+        public List<ActuatorAndJointNamePair> PairedActuatorAndJoints {
+            get => _pairedActuatorAndJoints;
+            set
+            {
+                _pairedActuatorAndJoints = value;
+                NotifyPropertyChanged();
+            }
+        }
+        
+        private void OnRobotConfiguredUpdated(object? sender, EventArgs e)
+        {
+            SetupDefaultActuatorToJointMapping();
+        }
+
+        private void SetupDefaultActuatorToJointMapping()
+        {
+            if (!_robotStateService.Robot.IsConfigured)
+                return;
+
+            AvailableActuators = [.. _robotStateService.Robot.GetAllActuators()];
+            IEnumerable<string>? AvailableUrdfJoints = null;
+
+            if (_robot != null)
+                AvailableUrdfJoints = _robot.Joints.Where(j => j.Value.IsMovable).Select(j => j.Value.Name);
+
+            PairedActuatorAndJoints = DefaultActuatorToJointMapping(AvailableActuators, AvailableUrdfJoints);
+        }
+
+        private List<ActuatorAndJointNamePair> DefaultActuatorToJointMapping(IEnumerable<Actuator> actuators, IEnumerable<string>? movableJoints)
+        {
+            List<ActuatorAndJointNamePair> pairs = new();
+
+            if (movableJoints == null)
+                return pairs;
+
+            int actuatorsCount = actuators.Count();
+
+            for(int i=0; i < movableJoints.Count(); i++)
+            {
+                pairs.Add(new ActuatorAndJointNamePair(
+                    actuator: i < actuatorsCount ? actuators.ElementAt(i) : null,
+                    jointName: movableJoints.ElementAt(i)
+                    )
+                );
+            }
+            return pairs;
+        }
+
+        private List<string> _availableJoints = new();
+        public List<string> AvailableJoints
+        {
+            get => _availableJoints;
+            set
+            {
+                _availableJoints = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
         private void OnRobotStateUpdated(object? sender, EventArgs e)
         {
             System.Windows.Application.Current.Dispatcher.Invoke(ComputeAndApplyTransforms);
@@ -149,13 +263,26 @@ namespace ControlPanel.Presentation.WPF.ViewModels
             if (_robot == null)
                 return;
 
-            //TODO: implement mapping URDF joints to RobotState joints
-            JointState[]? jointStates = Array.Empty<JointState>();
+            HashSet<JointState> jointStatesForActualAngle = new();
+            HashSet<JointState> jointStatesForTargetAngle = new();
+            foreach (ActuatorAndJointNamePair actuator in PairedActuatorAndJoints)
+            {
+                jointStatesForActualAngle.Add( new JointState {JointName= actuator.JointName, AngleDegrees=actuator.Acutator?.GetCurrentAngle ?? 0.0});
+                jointStatesForTargetAngle.Add( new JointState {JointName= actuator.JointName, AngleDegrees=actuator.Acutator?.GetTargetAngle() ?? 0.0});
+            }
 
-            var transformations = _forwardKinematicsService.Compute(_robot, jointStates);
+            var transformations = _forwardKinematicsService.Compute(_robot, jointStatesForActualAngle.ToList());
+            var transformationsForGhost = _forwardKinematicsService.Compute(_robot, jointStatesForTargetAngle.ToList());
 
-            foreach (var t in transformations) {
-                if (_linkGroups.TryGetValue(t.VisualKey, out var group))
+            ApplyTransformations(transformations, _linkGroups);
+            ApplyTransformations(transformationsForGhost, _ghostLinkGroups);
+        }
+
+        private void ApplyTransformations(IEnumerable<LinkTransformation> transformations, Dictionary<string, Model3DGroup> modelGroup)
+        {
+            foreach (var t in transformations)
+            {
+                if (modelGroup.TryGetValue(t.VisualKey, out var group))
                     group.Transform = new MatrixTransform3D(ToMatrix3D(t.WorldMatrix));
             }
         }
@@ -173,8 +300,7 @@ namespace ControlPanel.Presentation.WPF.ViewModels
         private int CountMovable()
         {
             return _robot?.Joints.Values.Count(j => j.IsMovable) ?? 0;
-        }
-
+        }        
 
     }
 }
